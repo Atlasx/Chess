947943384f316834bb9e3b5ae68097980ef93bf6
Atlasx <cjdouglas96@gmail.com>
2025-06-09 14:01:59 -1000

    Minor edits

--- Source/ChessGame/Private/Board/BoardSubsystem.cpp
+++ Source/ChessGame/Private/Board/BoardSubsystem.cpp
@@ -3,3 +3,158 @@
 
 #include "Board/BoardSubsystem.h"
 
+/*
+* Board Subsystem
+*/
+void UBoardSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		ActorSpawnedHandle = World->AddOnActorSpawnedHandler(FOnActorSpawned::FDelegate::CreateUObject(this, &UBoardSubsystem::OnActorSpawned));
+	}
+}
+
+void UBoardSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UBoardSubsystem::RegisterBoard(ABoardActor* Board)
+{
+
+}
+
+void UBoardSubsystem::UnregisterBoard(ABoardActor* Board)
+{
+
+}
+
+void UBoardSubsystem::ConnectBoards(ABoardActor* First, ABoardActor* Second)
+{
+
+}
+
+void UBoardSubsystem::DisconnectBoards(ABoardActor* First, ABoardActor* Second)
+{
+
+}
+
+void UBoardSubsystem::OnActorSpawned(AActor* SpawnedActor)
+{
+	if (ABoardActor* Board = Cast<ABoardActor>(SpawnedActor))
+	{
+		RegisterBoard(Board);
+	}
+}
+
+/*
+* Board Actor
+*/
+
+void ABoardActor::BuildGraph()
+{
+
+}
+
+
+
+void ABoardActor::RebuildGraph()
+{
+
+}
+
+void ABoardActor::ConnectNodes(const FBoardNode& First, const FBoardNode& Second, EBoardDirection Dir)
+{
+
+}
+
+void ABoardActor::DisconnectNodes(const FBoardNode& First, const FBoardNode& Second)
+{
+
+}
+
+void ABoardActor::ConnectExternal(ABoardActor* Other)
+{
+
+}
+
+void ABoardActor::DisconnectExternal(ABoardActor* Other)
+{
+
+}
+
+FBoardNode* ABoardActor::GetNodeAtLocation(const FVector& Location, float Tolerance /*= 50.f*/) const
+{
+	return nullptr;
+}
+
+FBoardNode* ABoardActor::GetClosestNode(const FVector& Location) const
+{
+	return nullptr;
+}
+
+bool ABoardActor::HasPath(const FBoardNode* From, const FBoardNode* To) const
+{
+	return false;
+}
+
+bool ABoardActor::FindPath(const FBoardNode* Start, const FBoardNode* End, TArray<const FBoardNode*>& OutPath) const
+{
+	return false;
+}
+
+void ABoardActor::DrawDebugGraph() const
+{
+	const float NodeDrawSize = 20.f;
+	const UWorld* World = GetWorld();
+
+	for (const FBoardNode& node : GraphNodes)
+	{
+		// Draw Node
+		FColor NodeColor = FColor::Red;
+		if (node.Flags & static_cast<uint8>(ENodeFlags::Navigable)) { NodeColor = FColor::Blue; }
+		DrawDebugSphere(World, node.Position, NodeDrawSize, 12, NodeColor);
+
+		// Draw Connections
+		for (uint8 dir = 0; dir < static_cast<uint8>(EBoardDirection::MAX); ++dir)
+		{
+			FBoardNode* other = nullptr;
+			EBoardDirection nodeDir = static_cast<EBoardDirection>(dir);
+			if (GetConstNodeNeighbor(node, nodeDir, other) == false) { continue; }
+			
+			DrawDebugLine(World, node.Position, other->Position, FColor::Blue);
+		}
+	}
+}
+
+void ABoardActor::DrawDebugPath(const TArray<FVector>& Path, FColor Color /*= FColor::Green*/) const
+{
+	
+}
+
+inline bool ABoardActor::GetNodeNeighbor(const FBoardNode& node, const EBoardDirection& direction, const FBoardNode& OutNode)
+{
+	int32 otherIndex = node.Neighbors[static_cast<uint8>(direction)];
+	if (otherIndex >= GraphNodes.Num())
+	{ 
+		OutNode = nullptr;
+		return false;
+	}
+
+	OutNode = &GraphNodes[otherIndex];
+	return OutNode != nullptr;
+}
+
+bool ABoardActor::GetConstNodeNeighbor(const FBoardNode& node, const EBoardDirection& direction, const FBoardNode* OutNode) const
+{
+	FBoardNode* OtherNode;
+	if (GetNodeNeighbor(node, direction, OtherNode))
+	{
+		OutNode = const_cast<const FBoardNode*>(OtherNode);
+		return true;
+	}
+	return false;
+}
--- Source/ChessGame/Public/Board/BoardSubsystem.h
+++ Source/ChessGame/Public/Board/BoardSubsystem.h
@@ -7,6 +7,87 @@
 
 #include "BoardSubsystem.generated.h"
 
+enum class EBoardDirection : uint8
+{
+    North,
+    East,
+    South,
+    West,
+    Northwest,
+    Northeast,
+    Southwest,
+    Southeast,
+    MAX
+};
+
+enum class ENodeFlags : uint8
+{
+    None = 0 UMETA(Hidden),
+    Navigable = 1 << 0
+};
+ENUM_CLASS_FLAGS(ENodeFlags)
+
+USTRUCT()
+struct FBoardNode
+{
+    GENERATED_BODY()
+
+    UPROPERTY()
+    FVector Position;
+
+    UPROPERTY()
+    uint8 Flags;
+
+    UPROPERTY()
+    int32 Neighbors[static_cast<uint8>(EBoardDirection::MAX)];
+};
+
+UCLASS()
+class CHESSGAME_API ABoardActor : public AActor
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere)
+    TArray<FBoardNode> GraphNodes;
+
+    TArray<FBoardNode*> ExternalConnections;
+
+public:
+    /* Connection Management */
+    UFUNCTION(CallInEditor)
+    void BuildGraph();
+
+    UFUNCTION(BlueprintCallable, CallInEditor)
+    void RebuildGraph();
+
+    void ConnectNodes(const FBoardNode& First, const FBoardNode& Second, EBoardDirection Dir);
+    void DisconnectNodes(const FBoardNode& First, const FBoardNode& Second);
+
+    UFUNCTION(BlueprintCallable)
+    void ConnectExternal(ABoardActor* Other);
+
+    UFUNCTION(BlueprintCallable)
+    void DisconnectExternal(ABoardActor* Other);
+
+    /* Navigation Requests */
+    FBoardNode* GetNodeAtLocation(const FVector& Location, float Tolerance = 50.f) const;
+    FBoardNode* GetClosestNode(const FVector& Location) const;
+
+    bool HasPath(const FBoardNode* From, const FBoardNode* To) const;
+    bool FindPath(const FBoardNode* Start, const FBoardNode* End, TArray<const FBoardNode*>& OutPath) const;
+
+    /* Debug */
+    UFUNCTION(CallInEditor)
+    void DrawDebugGraph() const;
+
+    UFUNCTION(CallInEditor)
+    void DrawDebugPath(const TArray<FVector>& Path, FColor Color = FColor::Green) const;
+
+protected:
+    inline bool GetNodeNeighbor(const FBoardNode& node,const EBoardDirection& direction, FBoardNode* OutNode);
+    inline bool GetConstNodeNeighbor(const FBoardNode& node, const EBoardDirection& direction, const FBoardNode* OutNode) const;
+};
+
 /**
  * 
  */
@@ -15,6 +96,26 @@ class CHESSGAME_API UBoardSubsystem : public UWorldSubsystem
 {
 	GENERATED_BODY()
 	
+    UPROPERTY()
+    TArray<ABoardActor*> Boards;
 
+    FDelegateHandle ActorSpawnedHandle;
+
+public:
+    /* UWorldSubsystem */
+    void Initialize(FSubsystemCollectionBase& Collection) override;
+    void Deinitialize() override;
+
+    /* Adding New Boards */
+    void RegisterBoard(ABoardActor* Board);
+    void UnregisterBoard(ABoardActor* Board);
+    void OnActorSpawned(AActor* SpawnedActor);
+
+    /* Connecting Boards Together */
+    void ConnectBoards(ABoardActor* First, ABoardActor* Second);
+    void DisconnectBoards(ABoardActor* First, ABoardActor* Second);
+
+    // Handle any navigation requests from actors with a criteria to fulfill
+    //void GetNavigableNodes(FVector Start, TArray<FBoardNode*>& Nodes, CriteriaFunc);
 
 };
